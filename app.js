b5dce5c47e6f960b9fd8e755d5ddd51a3da8c164
Pratik <pratiksharma20022@gmail.com>
2024-05-02 14:54:32 +0530

    added new files

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // This is a new File to be commited.
+
+console.log("Adding after edit in browser");
+console.log("Added in browser");
+
+const app = require('express')();
+app.get("/", (req,res) => {
+    res.json({ message : "cicd server g13 running" });
+})
+app.listen(4000, () => {
+    console.log(("Server running on 7000"));
+})
